57dc3b6daefca21838ae807759f66e95bdda08d6
Kevin <tehennepek@gmail.com>
2018-05-15 20:38:32 +0200

    Folder setup and navigation added

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Ralphouse</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Import CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />    
+</head>
+<body>
+    <?php include("html/navbar.html"); ?>
+    
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/bootstrap.js"> </script>
+</body>
+</html>
